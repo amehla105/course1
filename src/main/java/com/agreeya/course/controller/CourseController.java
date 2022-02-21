35c3fde75df638bb77b0d750aba48521e002deb4
Anil Kumar <79670700+amehla105@users.noreply.github.com>
2022-02-21 09:33:11 +0530

    Update CourseController.java

--- src/main/java/com/agreeya/course/controller/CourseController.java
+++ src/main/java/com/agreeya/course/controller/CourseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RequestMapping("/course")
 
 public class CourseController
@@ -17,31 +17,31 @@ public class CourseController
     @Autowired
     private CourseService courseService;
 
-    @PostMapping("/savecourse")
+    @PostMapping("/save")
     public Course saveCourseDetails(@RequestBody Course course)
     {
         return courseService.saveCourseDetails(course);
     }
 
-    @GetMapping("/findallcourses")
+    @GetMapping("/findall")
     public List<Course> getAllCourseDetails()
     {
         return courseService.getAllCourseDetails();
     }
 
-    @GetMapping("/findbycourseid/{id}")
+    @GetMapping("/findbyid/{id}")
     public Course findByCourseIdDetails(@PathVariable("id") long courseId)
     {
         return courseService.findByCourseIdDetails(courseId);
     }
 
-    @DeleteMapping("deletebycourseid/{id}")
+    @DeleteMapping("deletebyid/{id}")
     public void deleteCourseById(@PathVariable("id") long courseId)
     {
         courseService.deleteCourseById(courseId);
     }
 
-    @PutMapping("/updatecoursebyid/{id}")
+    @PutMapping("/updatebyid/{id}")
     public Course updateCourseById(@RequestBody Course course,@PathVariable("id") long courseId)
     {
         return courseService.updateCourseById(course,courseId);
